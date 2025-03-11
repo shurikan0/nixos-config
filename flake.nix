558c1d6a39460ae15bdadd670f7264ceebb56d7f
Jan Gruhnert <36522183+shurikan0@users.noreply.github.com>
2025-03-11 15:29:07 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,16 +1,17 @@
 {
-  description = "Flake of LibrePhoenix";
+  description = "Flake of Shurikan's NixOS configuration 
+  currently forked from Phoenix's NixOS configuration";
 
   outputs = inputs@{ self, ... }:
     let
       # ---- SYSTEM SETTINGS ---- #
       systemSettings = {
         system = "x86_64-linux"; # system arch
-        hostname = "snowfire"; # hostname
+        hostname = "shurikan"; # hostname
         profile = "personal"; # select a profile defined from my profiles directory
-        timezone = "America/Chicago"; # select timezone
-        locale = "en_US.UTF-8"; # select locale
-        keyboard_locale = "de_DE.UTF-8";
+        timezone = "Europe/Berlin"; # select timezone
+        locale = "de_DE.UTF-8"; # select locale
+        keyboard = "de"; # select keyboard layout
         bootMode = "uefi"; # uefi or bios
         bootMountPath = "/boot"; # mount path for efi boot partition; only used for uefi boot mode
         grubDevice = ""; # device identifier for grub; only used for legacy (bios) boot mode
@@ -19,16 +20,16 @@
 
       # ----- USER SETTINGS ----- #
       userSettings = rec {
-        username = "emmet"; # username
-        name = "Emmet"; # name/identifier
-        email = "emmet@librephoenix.com"; # email (used for certain configurations)
+        username = "shurikan"; # username
+        name = "jan"; # name/identifier
+        email = "jan-gruhnert@startmail.com"; # email (used for certain configurations)
         dotfilesDir = "~/.dotfiles"; # absolute path of the local repo
-        theme = "io"; # selcted theme from my themes directory (./themes/)
+        theme = "ashes"; # selcted theme from my themes directory (./themes/)
         wm = "hyprland"; # Selected window manager or desktop environment; must select one in both ./user/wm/ and ./system/wm/
         # window manager type (hyprland or x11) translator
-        wmType = if ((wm == "hyprland") || (wm == "plasma")) then "wayland" else "x11";
-        browser = "qutebrowser"; # Default browser; must select one from ./user/app/browser/
-        spawnBrowser = if ((browser == "qutebrowser") && (wm == "hyprland")) then "qutebrowser-hyprprofile" else (if (browser == "qutebrowser") then "qutebrowser --qt-flag enable-gpu-rasterization --qt-flag enable-native-gpu-memory-buffers --qt-flag num-raster-threads=4" else browser); # Browser spawn command must be specail for qb, since it doesn't gpu accelerate by default (why?)
+        wmType = "wayland";
+        browser = "brave"; # Default browser; must select one from ./user/app/browser/
+        spawnBrowser = "brave"; # Default browser spawning translator
         defaultRoamDir = "Personal.p"; # Default org roam directory relative to ~/Org
         term = "alacritty"; # Default terminal command;
         font = "Intel One Mono"; # Selected font
