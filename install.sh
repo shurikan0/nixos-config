55f640fba26b3af5f32bf23c27faab8751106774
shurikan0 <36522183+shurikan0@users.noreply.github.com>
2025-03-11 21:21:14 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ if [ $# -gt 0 ]
   else
     SCRIPT_DIR=~/.dotfiles
 fi
-nix-shell -p git --command "git clone https://gitlab.com/librephoenix/nixos-config $SCRIPT_DIR"
+nix-shell -p git --command "git clone https://github.com/shurikan0/nixos-config.git $SCRIPT_DIR"
 
 # Generate hardware config for new system
 sudo nixos-generate-config --show-hardware-config > $SCRIPT_DIR/system/hardware-configuration.nix
@@ -24,9 +24,9 @@ else
 fi
 
 # Patch flake.nix with different username/name and remove email by default
-sed -i "0,/emmet/s//$(whoami)/" $SCRIPT_DIR/flake.nix
-sed -i "0,/Emmet/s//$(getent passwd $(whoami) | cut -d ':' -f 5 | cut -d ',' -f 1)/" $SCRIPT_DIR/flake.nix
-sed -i "s/emmet@librephoenix.com//" $SCRIPT_DIR/flake.nix
+sed -i "0,/shurikan/s//$(whoami)/" $SCRIPT_DIR/flake.nix
+sed -i "0,/Jan/s//$(getent passwd $(whoami) | cut -d ':' -f 5 | cut -d ',' -f 1)/" $SCRIPT_DIR/flake.nix
+sed -i "s/jan-gruhnert@startmail.com//" $SCRIPT_DIR/flake.nix
 sed -i "s+~/.dotfiles+$SCRIPT_DIR+g" $SCRIPT_DIR/flake.nix
 
 # Open up editor to manually edit flake.nix before install
